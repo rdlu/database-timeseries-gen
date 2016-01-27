6533a88e1d70afd8a0d41d7c55c94ee769e9b1a9
Rodrigo Dlugokenski <rddweb@gmail.com>
2016-01-27 17:53:45 -0200

    More work on sql and dns

--- lib/psql.rb
+++ lib/psql.rb
@@ -4,14 +4,26 @@ class Psql
     CREATE DATABASE `rdlu` OWNER `rdlu` ENCODING `UTF8`;"
   end
 
-  def self.create_table(table, filters)
-    "CREATE TABLE #{table} (`timestamp` timestamp)"
+  def self.create_table(table, filters, values, index, size = 128)
+    "CREATE TABLE #{table} (`timestamp` timestamp NOT NULL, #{ filters.map do |f| "`#{f[0]}` #{f[1]}" end.join(', ') }, #{ values.map do |f| "`#{f[0]}` #{f[1]}" end.join(', ')});\n" << case index
+    when 'brin'
+      "CREATE INDEX idx_ts_brin_#{size} ON #{table} USING BRIN (timestamp) WITH (pages_per_range = #{size});"
+    when 'btree'
+      "CREATE INDEX idx_ts_btree ON #{table} USING BTREE (timestamp);"
+    else
+      ""
+    end
   end
+
   def self.insert(table, data)
     "INSERT INTO #{table} (#{data.keys.join(',')}) VALUES ('#{data.values.join('\',\'')}');"
   end
 
   def self.select(table, timerange, filters)
-    "SELECT * FROM #{table} WHERE timestamp BETWEEN AND "
+    "SELECT * FROM #{table} WHERE (timestamp BETWEEN #{timerange[:start]} AND #{timerange[:end]})" << if filters.to_a.count > 0
+      " AND (#{ filters.map do |f| "`#{f[0]}` = #{f[1]}" end.join(' AND ') });"
+    else
+      ";"
+    end
   end
 end
